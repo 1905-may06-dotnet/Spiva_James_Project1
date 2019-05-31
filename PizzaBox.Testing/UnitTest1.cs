7ce22c3a7e9c68bcbb977952db4666022767738d
James Spiva <zspiva@gmail.com>
2019-05-31 09:00:48 -0500

    Forgot to create it before now

---
+++ PizzaBox.Testing/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PizzaBox.Context;
+using PizzaBox.Context.DataModel;
+using PizzaBox.Context.Repositories;
+using PizzaBox.Library;
+using PizzaBox.Library.DataClasses;
+using PizzaBox.Library.Repositories;
+
+namespace PizzaBox.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            RestaurantRepository repo = new RestaurantRepository();
+            Restaurant r = repo.Read(1);
+        }
+    }
+}
